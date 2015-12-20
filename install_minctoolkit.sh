7564fe762cca3287d779ed0ac752b0b1e22ae634
datamancy <cfhammill@gmail.com>
2015-12-20 10:02:53 -0500

    suppress even more output

--- install_minctoolkit.sh
+++ install_minctoolkit.sh
@@ -6,6 +6,6 @@ cd minc-toolkit
 mkdir build 
 cd build
 echo building minc-toolkit
-cmake .. > /dev/null
-make > /dev/null
-make install /dev/null
+cmake .. &>/dev/null
+make &>/dev/null
+make install &>/dev/null
