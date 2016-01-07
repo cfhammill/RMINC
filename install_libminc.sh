2cb22c2caaaffbe8423269fc25e5a5e427e5bbf3
cfhammill <cfhammill@gmail.com>
2016-01-07 14:40:41 -0500

    remove duplicate cmake

--- install_libminc.sh
+++ install_libminc.sh
@@ -3,6 +3,6 @@
 cd ..
 git clone https://github.com/BIC-MNI/libminc
 cd libminc
-cmake cmake -DLIBMINC_BUILD_SHARED_LIBS=ON -DLIBMINC_MINC1_SUPPORT=ON . 
+cmake -DLIBMINC_BUILD_SHARED_LIBS=ON -DLIBMINC_MINC1_SUPPORT=ON . 
 make
 sudo make install
