8d0de3207c0f23c6a418447a50f2370b7d4f65a2
cfhammill <cfhammill@gmail.com>
2016-01-08 11:58:06 -0500

    Cleaned up travis install

---
+++ travis_install_scripts/install_apt_dependencies.sh
@@ -0,0 +1,16 @@
+# remove special apt repositories to install hdf5
+sudo add-apt-repository -ry "deb http://cran.rstudio.com//bin/linux/ubuntu $(lsb_release -cs)/"
+sudo add-apt-repository -ry "ppa:marutter/c2d4u"
+ 
+sudo apt-get update -qq
+sudo apt-get install -y libvtk5-dev \
+                        python-dev \
+                        python-vtk \
+                        libssh2-1-dev \
+                        libcurl4-openssl-dev \
+                        libhdf5-serial-dev \
+
+ # Add them back for downstream stuff
+ sudo add-apt-repository -y "deb http://cran.rstudio.com//bin/linux/ubuntu $(lsb_release -cs)/"
+ sudo add-apt-repository -y "ppa:marutter/c2d4u"
+ sudo apt-get update -qq
